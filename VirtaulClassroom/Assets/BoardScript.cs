3373f7b6a20f25da206e3e843659a778f18258fd
roy8gn <roygnr@gmail.com>
2021-11-02 17:01:50 +0200

    Updated Board script

--- VirtaulClassroom/Assets/BoardScript.cs
+++ VirtaulClassroom/Assets/BoardScript.cs
@@ -6,15 +6,40 @@ using UnityEngine;
 public class BoardScript : MonoBehaviour
 {
     public TextMeshPro text;
+    public Lesson lesson;
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponentInChildren<TextMeshPro>();
-        text.text = "Welcome to VR Classroom!";
+        lesson = new Lesson();
+        text.text = lesson.words[0].ForiegnWord + " " + lesson.words[0].EnglishTranslation;
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+}
+
+public class Word
+{
+    public string ForiegnWord { get; set; }
+    public string EnglishTranslation { get; set; }
+
+    public Word(string foriegnWord, string englishTranslation)
+    {
+        ForiegnWord = foriegnWord;
+        EnglishTranslation = englishTranslation;
+    }
+}
+
+public class Lesson
+{
+    public Word[] words;
+    public Lesson()
+    {
+        words = new Word[2];
+        words[0] = new Word("Kelev", "Dog");
+        words[1] = new Word("Hatul", "Cat");
     }
 }
